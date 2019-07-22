8e0931e3c6e831973435688056f4f5b439875ab8
Vlad Borodaev <nikoskefali@gmail.com>
2019-07-22 17:39:18 +0300

    add simple form for example

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
+var validator = require('express-validator');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('form', { title: 'Registration form' });
 });
+
+router.post('/',
+  // validators
+  [
+    validator
+      .body('name')
+      .isLength({ min: 1 })
+      .withMessage('Please enter a name'),
+    validator
+      .body('email')
+      .isLength({ min: 1 })
+      .withMessage('Please enter an email'),
+  ],
+  function(req, res) {
+    var errors = validator.validationResult(req);
+    if (errors.isEmpty()) {
+      res.send('Successfully registered');
+    } else {
+      res.render('form', {
+        title: 'Registration form',
+        errors: errors.array(),
+        data: req.body,
+      });
+    }
+  }
+);
 
 module.exports = router;
